024a253137a530a0b354c0d5810f5ceb804a26fe
John Beech <github@mkv25.net>
2015-07-15 22:42:16 +0100

    Added colors to test-compilation.js

--- test-compilation.js
+++ test-compilation.js
@@ -1,14 +1,28 @@
+// Colors
+var colors = require('colors');
+
 // Compile
 var compiler = require('../web-component-js').compiler;
-compiler.compile('./tests/compiler/source/*.component.html', './tests/compiler/build', compilationComplete);
+compiler.compile('./tests/compiler/source/*.component.html', './tests/compiler/build', compilationComplete, onUpdate);
 
 function compilationComplete(error, success) {
   if (success) {
-    console.log('Compilation succeeded', success);
+    console.log('Compilation succeeded'.green, success);
     compare();
   } else {
-    console.log('Compilation failed: ', error);
+    console.log('Compilation failed: '.red, error);
   }
+}
+
+function onUpdate(message, type) {
+  var color = {
+    info: 'cyan',
+    warning: 'yellow',
+    error: 'red',
+    success: 'green',
+    plain: 'white'
+  }[type] || 'grey';
+  console.log(message[color]);
 }
 
 function compare() {
@@ -18,12 +32,12 @@ function compare() {
     fileContents: true
   }, function (error, diffs) {
     if (error) {
-      console.log('Errors: ', error);
+      console.log('Errors: '.red, error);
     } else if (diffs.length > 0) {
-      console.log('Differences: ');
+      console.log('Differences found between canned and compiled files: '.red);
       console.log(diffs);
     } else {
-      console.log('No differences found between canned and build directories.');
+      console.log('No differences found between canned and build directories.'.green);
     }
   });
 }
